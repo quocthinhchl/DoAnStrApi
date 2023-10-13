5c06b68f99b7522c4aa331d7d8a19a51291acace
quocthinhchl <quocthinh.11ly.chl@gmail.com>
2023-10-13 23:05:36 +0700

    fix

--- src/api/borrow-history/routes/borrow-history.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * borrow-history router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::borrow-history.borrow-history');
--- src/api/borrow-history/services/borrow-history.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * borrow-history service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::borrow-history.borrow-history');
